ca0743e12b1ede61f9ffaf5d9a00226f92b8073e
Muhammad Hafiz Ansari <mhmmdhafizansari12@gmail.com>
2024-11-05 16:33:14 +0800

    feat: get posts and get post by id endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import { body } from 'express-validator'
-import { createPost } from '../controllers/post.js'
+import { createPost, getPosts, getPostById } from '../controllers/post.js'
 import { upload } from '../middlewares/upload.js'
 import { validateRequest } from '../middlewares/validateRequest.js'
 
 const router = express.Router()
 
+router.get(
+    '/post',
+    getPosts
+)
+router.get(
+    '/post/:id',
+    getPostById
+)
 router.post(
     '/post',
     upload.single('image'),
