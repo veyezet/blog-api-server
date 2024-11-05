cf0a30ee8adf72cf7a94e4d09d59cc5e4898ffda
Muhammad Hafiz Ansari <mhmmdhafizansari12@gmail.com>
2024-11-05 17:09:45 +0800

    feat: update and delete blog post endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { body } from 'express-validator'
-import { createPost, getPosts, getPostById } from '../controllers/post.js'
+import { createPost, getPosts, getPostById, updatePost, deletePost } from '../controllers/post.js'
 import { upload } from '../middlewares/upload.js'
 import { validateRequest } from '../middlewares/validateRequest.js'
 
@@ -23,6 +23,20 @@ router.post(
     ],
     validateRequest,
     createPost
+)
+router.put(
+    '/post/:id',
+    upload.single('image'),
+    [
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('body').not().isEmpty().withMessage('Body is required')
+    ],
+    validateRequest,
+    updatePost
+)
+router.delete(
+    '/post/:id',
+    deletePost
 )
 
 export default router
