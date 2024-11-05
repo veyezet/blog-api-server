a26f9df7c3a6180462619035de7d36fabf5691dc
Muhammad Hafiz Ansari <mhmmdhafizansari12@gmail.com>
2024-11-05 16:04:03 +0800

    feat: create blog post endpoint validation

---
+++ src/middlewares/validateRequest.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+
+export const validateRequest = (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            message: "Bad Request!",
+            errors: errors.array()
+        })
+    }
+
+    next()
+}
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import { body } from 'express-validator'
 import { createPost } from '../controllers/post.js'
 import { upload } from '../middlewares/upload.js'
+import { validateRequest } from '../middlewares/validateRequest.js'
 
 const router = express.Router()
 
 router.post(
     '/post',
     upload.single('image'),
+    [
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('body').not().isEmpty().withMessage('Body is required')
+    ],
+    validateRequest,
     createPost
 )
 
